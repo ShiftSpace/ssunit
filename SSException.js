f13a1de27eaa0bfbf68f61dd5324073bc665e4ff
David Nolen <david.nolen@gmail.com>
2009-09-03 14:16:24 -0400

    added index.html, license and readme.

--- SSException.js
+++ SSException.js
@@ -5,57 +5,24 @@
 // ==/Builder==
 
 var SSExceptionPrinter = new Class({
-  toString: function()
-  {
+  toString: function() {
     return ["["+this.name+"] message: " + this.message(), " fileName:" + this.fileName(), " lineNumber: " + this.lineNumber(), (this.originalError() && this.originalError().message) || 'no original error'].join(", ");
   }
 });
 
 var SSException = new Class({
-  
-  name: 'SSException',
-
   Implements: SSExceptionPrinter,
-  
-  initialize: function(_error)
-  {
-    this.theError = _error;
-  },
-    
-  setMessage: function(msg)
-  {
-    this.__message = msg; 
-  },
-  
-  message: function()
-  {
-    return this.__message || (this.theError && this.theError.message) || 'n/a';
-  },
-  
-  fileName: function()
-  {
-    return (this.theError && this.theError.fileName) || 'n/a';
-  },
-
-  lineNumber: function()
-  {
-    return (this.theError && this.theError.lineNumber) || 'n/a';
-  },
-  
-  originalError: function()
-  {
-    return this.theError;
-  }
-  
+  name: 'SSException',
+  initialize: function(_error) { this.theError = _error; },
+  setMessage: function(msg) { this.__message = msg; },
+  message: function() { return this.__message || (this.theError && this.theError.message) || 'n/a'; },
+  fileName: function() { return (this.theError && this.theError.fileName) || 'n/a'; },
+  lineNumber: function() { return (this.theError && this.theError.lineNumber) || 'n/a'; },
+  originalError: function() { return this.theError; }
 });
 
-
-function SSDescribeException(_exception)
-{
+function SSDescribeException(_exception) {
   var temp = [];
-  for(var prop in _exception)
-  {
-    temp.push(prop + ':' + _exception[prop]);
-  }
+  for(var prop in _exception) temp.push(prop + ':' + _exception[prop]);
   return "Exception:{ " + temp.join(', ') +" }";
 }
