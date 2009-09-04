6def35871c15e22f692ecbef5578a62230b534b3
David Nolen <david.nolen@gmail.com>
2009-09-03 20:44:08 -0400

    added $arity.

--- notes.js
+++ notes.js
@@ -1,20 +1,50 @@
+/*
+"function $9hello(a, b) { cool(); }(".match(/function \S*\((.*?)\)/)
+var sum = $arity(
+  function(a) { return a };
+  function(a, b) { return a + b.first(); };
+)
+*/
+function $arity()
+{
+  var fns = $A(arguments);
+  var dispatch = [];
+  fns.each(function(fn) {
+    var arglist = fn.toString().match(/function \S*\((.*?)\)/)[1].split(',');
+    dispatch[arglist.length] = fn;
+  });
+  return function () {
+    var args = $A(arguments);
+    return dispatch[args.length].apply(this, args);
+  }
+}
+
+var sum = $arity(
+  function(a) { return a; },
+  function(a, b) { return a + (($type(b) == 'array') ? b.first() : b); }
+);
+
 var SSUnit = {};
 SSUnit.assertEqual = function(a, b) {
   var caller = SSUnit.assertEqual.caller;
   console.log(caller.__name, caller.__doc, caller.__istest);
 };
 
-function deftest(doc, fn) {
+function $deftest(doc, fn) {
   fn.__doc = doc;
   fn.__istest = true;
   return fn;
 }
 
-function processTest(test) {
+function $doc(fn) { return fn.__doc; };
+function $name(fn) { return fn.__name; };
+function $origin(fn) { return fn._origin };
+
+function $processTest(test) {
   for(var field in test) {
     var v = test[field];
     if($type(v) == "function") {
-      var origin = v._origin;
+      var origin = $origin(v);
       if(origin.__istest) {
         v.__istest = true;
         v.__doc = origin.__doc;
@@ -27,7 +57,7 @@ function processTest(test) {
 }
 
 var unittest = new Class({
-  login: deftest(
+  login: $deftest(
     "Verify that login works",
     function() {
       SSUnit.assertEqual(3, 4);
