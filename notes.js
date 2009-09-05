d6ead641c17be93cf8c7a0190c81b6698b05f2f7
David Nolen <david.nolen@gmail.com>
2009-09-05 12:53:06 -0400

    Added mootools.

--- notes.js
+++
@@ -1,5 +0,0 @@
-var add = function(a, b) { return a + b; }.asPromise();
-var sum = $arity(
-  function(a) { return a; }.asPromise(),
-  function(a, b) { return add(a, (($type(b) == 'array') ? b.first() || 0 : b)); }.asPromise()
-);
