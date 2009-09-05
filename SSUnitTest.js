4fa6a4e6495398e7d85d3337f507fa6ef2be1997
David Nolen <david.nolen@gmail.com>
2009-09-05 12:52:35 -0400

    moved add and sum into SSUnitTest.js

--- SSUnitTest.js
+++ SSUnitTest.js
@@ -2,6 +2,12 @@
 // = Utilities =
 // ==============
 
+var add = function(a, b) { return a + b; }.asPromise();
+var sum = $arity(
+  function(a) { return a; }.asPromise(),
+  function(a, b) { return add(a, (($type(b) == 'array') ? b.first() || 0 : b)); }.asPromise()
+);
+
 if(!Array.copy) {
   Array.implement({
     copy: function() {
