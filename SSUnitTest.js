a9d1ddf1501cb85b186c5fd64225f49a64d9592f
dnolen <david.nolen@gmail.com>
2009-10-25 18:18:06 -0400

    Updated functools submodule. Removed bug in sum code.

--- SSUnitTest.js
+++ SSUnitTest.js
@@ -4,8 +4,8 @@
 
 var add = function(a, b) { return a + b; }.asPromise();
 var sum = $arity(
-  function(a) { return a; }.asPromise(),
-  function(a, b) { return add(a, (($type(b) == 'array') ? b.first() || 0 : b)); }.asPromise()
+  function(a) { return a; },
+  function(a, b) { return add(a, b.first()); }
 );
 
 if(!Array.copy) {
